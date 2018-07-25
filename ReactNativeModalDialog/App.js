774a56de7bf9efd0c12daf6cb9b1d26c0eec121b
zhengxiaoyao0716 <1499383852@qq.com>
2018-07-25 10:34:16 +0800

    resolved the warning that `this.refs was deprecated`.

--- ReactNativeModalDialog/App.js
+++ ReactNativeModalDialog/App.js
@@ -1,81 +1,81 @@
 import React, { Component } from 'react';
 import {
-    StyleSheet,
-    View, Text, TextInput,
+  StyleSheet,
+  View, Text, TextInput,
 } from 'react-native';
 
 import Dialog, { Alert, Confirm, Prompt, } from 'react-native-modal-dialog';
 
 export default class Example extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { text: '', };
-    }
-    render() {
-        return <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#eee', }}>
-            {/*Inject methods*/}
-            <Text style={styles.textButton} onPress={() => { this.refs.dialog.alert("message") }}>Alert</Text>
-            <Text style={styles.textButton} onPress={() => { this.refs.dialog.confirm("message") }}>Confirm</Text>
-            <Text style={styles.textButton} onPress={() => {
-                this.refs.dialog.prompt("message", "placeholder", (text) => { this.setState({ text, }) }, );
-            }}>Prompt:{this.state.text}</Text>
-            <Text style={styles.textButton} onPress={() => {
-                this.refs.dialog.alert(undefined, {
-                    title: "customTitle",
-                    titleStyle: { color: '#03A9F4', },
-                    children: <View><Text>customChildren</Text></View>,
-                    posText: 'customButton',
-                    buttonStyle: { color: '#03A9F4', },
-                    onPosClick: () => {
-                        this.refs.dialog.hide();
-                        this.refs.dialog.alert("customMessage", {
-                            messageStyle: { fontSize: 12, },
-                            title: undefined,
-                        });
-                    },
-                })
-            }}>Custom Alert</Text>
-            <Text style={styles.textButton} onPress={() => {
-                this.refs.dialog.confirm("must choose one, can't cancel", {
-                    onCancel: () => true,
-                    negText: 'multi-show',
-                    onNegClick: () => {
-                        this.refs.dialog.hide();
-                        this.refs.dialog.alert('First dialog.', { posText: 'Closed this to show second dialog.' });
-                        this.refs.dialog.alert('This is the second dialog.');
-                    },
-                    posText: 'scroll',
-                    onPosClick: () => {
-                        this.refs.dialog.hide();
-                        this.refs.dialog.alert((function counter(count) { return count ? counter(count - 1) + count + '\n' : '0\n'; })(30));
-                    },
-                    contentStyle: { backgroundColor: "#6cf"},
-                })
-            }}>Custom Confirm</Text>
+  constructor(props) {
+    super(props);
+    this.state = { text: '', };
+  }
+  render() {
+    return <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#eee', }}>
+      {/*Inject methods*/}
+      <Text style={styles.textButton} onPress={() => { this.dialog.alert("message") }}>Alert</Text>
+      <Text style={styles.textButton} onPress={() => { this.dialog.confirm("message") }}>Confirm</Text>
+      <Text style={styles.textButton} onPress={() => {
+        this.dialog.prompt("message", "placeholder", (text) => { this.setState({ text, }) }, );
+      }}>Prompt:{this.state.text}</Text>
+      <Text style={styles.textButton} onPress={() => {
+        this.dialog.alert(undefined, {
+          title: "customTitle",
+          titleStyle: { color: '#03A9F4', },
+          children: <View><Text>customChildren</Text></View>,
+          posText: 'customButton',
+          buttonStyle: { color: '#03A9F4', },
+          onPosClick: () => {
+            this.dialog.hide();
+            this.dialog.alert("customMessage", {
+              messageStyle: { fontSize: 12, },
+              title: undefined,
+            });
+          },
+        })
+      }}>Custom Alert</Text>
+      <Text style={styles.textButton} onPress={() => {
+        this.dialog.confirm("must choose one, can't cancel", {
+          onCancel: () => true,
+          negText: 'multi-show',
+          onNegClick: () => {
+            this.dialog.hide();
+            this.dialog.alert('First dialog.', { posText: 'Closed this to show second dialog.' });
+            this.dialog.alert('This is the second dialog.');
+          },
+          posText: 'scroll',
+          onPosClick: () => {
+            this.dialog.hide();
+            this.dialog.alert((function counter(count) { return count ? counter(count - 1) + count + '\n' : '0\n'; })(30));
+          },
+          contentStyle: { backgroundColor: "#6cf" },
+        })
+      }}>Custom Confirm</Text>
 
-            {/*Pre-build methods*/}
-            <Text style={styles.textButton} onPress={() => { this.refs.alert.show() }}>Pre-build Alert</Text>
-            <Text style={styles.textButton} onPress={() => { this.refs.confirm.show() }}>Pre-build Confirm</Text>
-            <Text style={styles.textButton} onPress={() => { this.refs.prompt.show(); }}>Pre-build Prompt:{this.state.text}</Text>
+      {/*Pre-build methods*/}
+      <Text style={styles.textButton} onPress={() => { this.alert.show() }}>Pre-build Alert</Text>
+      <Text style={styles.textButton} onPress={() => { this.confirm.show() }}>Pre-build Confirm</Text>
+      <Text style={styles.textButton} onPress={() => { this.prompt.show(); }}>Pre-build Prompt:{this.state.text}</Text>
 
-            {Dialog.inject()}
-            <Alert ref='alert' message='message'></Alert>
-            <Confirm ref='confirm' contentStyle={{ backgroundColor: "#6cf"}}><Text>children</Text></Confirm>
-            <Prompt ref='prompt'>
-                <TextInput placeholder='first' />
-                <TextInput placeholder='second' />
-            </Prompt>
-        </View>;
-    }
+      {Dialog.inject(dialog => this.dialog = dialog)}
+      <Alert ref={alert => this.alert = alert} message='message'></Alert>
+      <Confirm ref={confirm => this.confirm = confirm} contentStyle={{ backgroundColor: "#6cf" }}><Text>children</Text></Confirm>
+      <Prompt ref={prompt => this.prompt = prompt}>
+        <TextInput placeholder='first' />
+        <TextInput placeholder='second' />
+      </Prompt>
+    </View>;
+  }
 }
 
 const styles = StyleSheet.create({
-    textButton: {
-        fontSize: 24,
-        color: '#fff',
-        backgroundColor: '#03A9F4',
-        borderRadius: 8,
-        padding: 8,
-        margin: 8,
-    }
+  textButton: {
+    fontSize: 24,
+    color: '#fff',
+    backgroundColor: '#03A9F4',
+    borderRadius: 8,
+    padding: 8,
+    margin: 8,
+  }
 });
